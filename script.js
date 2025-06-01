d42124680f4677c13cb820ad3f34abe50bd4e6c3
Meeran <mee.sidd.1919@gmail.com>
2025-06-01 15:49:45 +0530

    modified index.html and script.js

--- script.js
+++ script.js
@@ -160,8 +160,10 @@ document.getElementById("emailForm").onsubmit = function (event) {
     );
 };
 
+/*
 // Need to update after completion-->
-document.getElementById('spotify-content').lastElementChild.addEventListener('click', function(event) {
-  event.preventDefault(); // prevents page reload
-  alert("🚧 This project is currently under development. Stay tuned for updates, and check out my other completed projects!");
-});
+// document.getElementById('spotify-content').lastElementChild.addEventListener('click', function(event) {
+//   event.preventDefault(); // prevents page reload
+//   alert("🚧 This project is currently under development. Stay tuned for updates, and check out my other completed projects!");
+// });
+*/
